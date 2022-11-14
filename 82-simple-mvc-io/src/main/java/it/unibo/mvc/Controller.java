5219caa396259281b02785a6debda4150701374a
Mattia Flamigni - mattia.flamigni4@studio.unibo.it <mattia.flamigni4@studio.unibo.it>
2022-11-14 12:45:31 +0100

    implemented method return current file

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,21 @@
 package it.unibo.mvc;
 
+import java.io.File;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    
+    private final static String HOME = System.getProperty("user.home");
+
+    private File file = new File (HOME+File.separator+"output.txt");
+
+    public File getCurrentFile(){
+        return file;
+    }   
+
+
+
 
 }
