52756ef7582ce34b7b74b0f108f4a6e54b8d10ab
Mattia Flamigni - mattia.flamigni4@studio.unibo.it <mattia.flamigni4@studio.unibo.it>
2022-11-14 13:33:56 +0100

    add text area and button

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,8 @@
 package it.unibo.mvc;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+
+import javax.swing.*;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +11,18 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+
+    private SimpleGUI(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JPanel canvas = new JPanel();
+        final JButton save = new JButton("save");
+        final JTextArea text = new JTextArea();
+
+        canvas.add(text, BorderLayout.SOUTH);
+        canvas.add(save, BorderLayout.CENTER);
+
+
+
+    }
 
 }
