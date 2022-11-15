20ab949c18c117fea9b3663ddf66b49b2fca3a87
mattia <flami2002@gmail.com>
2022-11-15 23:38:23 +0100

    set the gui element (a lot of difficult with the position of the buttons)

--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,7 @@
 package it.unibo.mvc;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +10,36 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+
+    public SimpleGUI(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel canvas = new JPanel();
+        JPanel canvasSouth = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        canvasSouth.setLayout(new BoxLayout(canvasSouth, BoxLayout.LINE_AXIS));
+
+        JTextArea textArea = new JTextArea();
+        JTextField textField = new JTextField();
+        JButton print = new JButton("Print");
+        JButton viewHistory = new JButton("View History");
+
+        frame.setContentPane(canvas);
+        canvas.add(textField, BorderLayout.NORTH);
+        canvas.add(textArea, BorderLayout.CENTER);
+        canvas.add(canvasSouth, BorderLayout.SOUTH);
+        canvasSouth.add(print);
+        canvasSouth.add(viewHistory);
+
+        
+    }
+
+    void display(){
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... a){
+        SimpleGUI gui = new SimpleGUI();
+        gui.display();
+    }
 
 }
