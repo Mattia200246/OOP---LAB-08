5835e9c13b79b3df9e31b6eae38298134d312a82
mattia <flami2002@gmail.com>
2022-11-15 22:36:21 +0100

    add implemented method

--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,35 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    @Override
+    public void setNextString(String string) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void getNextString() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<String> history() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void printCurrentString() {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
