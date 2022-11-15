afdfcd579002a49ad79f31fdaa060fe93ab85788
mattia <flami2002@gmail.com>
2022-11-15 10:00:43 +0100

    fixed problems about object too large, using suggestion in readme file

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -13,21 +13,20 @@ public final class SimpleGUIWithFileChooser {
     private final JFrame frame = new JFrame();
     
     public SimpleGUIWithFileChooser(Controller controller){
-        JPanel canvas = new JPanel();
-        JPanel canvas2 = new JPanel();
+        final JPanel canvas = new JPanel();
+        final JPanel canvas_up = new JPanel();
         canvas.setLayout(new BorderLayout());
-        canvas2.setLayout(new BorderLayout());
-        JTextArea text = new JTextArea();
-        JButton browse = new JButton("Browse...");
+        canvas_up.setLayout(new BorderLayout());
+        JTextField text = new JTextField();
+        text.setEditable(false);
+        final JButton browse = new JButton("Browse...");
         
-        //canvas.add(canvas1, BorderLayout.NORTH);
-        canvas2.add(canvas, BorderLayout.NORTH);
-        canvas2.add(text, BorderLayout.CENTER);
-        canvas2.add(browse, BorderLayout.LINE_END);
+        canvas_up.add(text, BorderLayout.CENTER);
+        canvas_up.add(browse, BorderLayout.LINE_END);
+        canvas.add(canvas_up, BorderLayout.NORTH);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(canvas);
-        frame.setContentPane(canvas2);
         
     }
 
