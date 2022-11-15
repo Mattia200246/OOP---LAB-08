65ecc071c695f9423e1cfbf24cdca68422da2868
mattia <flami2002@gmail.com>
2022-11-15 09:52:17 +0100

    created GUI with textfield and button browse. some problems: too big

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,6 +1,7 @@
 package it.unibo.mvc;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 
 /**
@@ -10,5 +11,33 @@ import javax.swing.JFrame;
 public final class SimpleGUIWithFileChooser {
 
     private final JFrame frame = new JFrame();
+    
+    public SimpleGUIWithFileChooser(Controller controller){
+        JPanel canvas = new JPanel();
+        JPanel canvas2 = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        canvas2.setLayout(new BorderLayout());
+        JTextArea text = new JTextArea();
+        JButton browse = new JButton("Browse...");
+        
+        //canvas.add(canvas1, BorderLayout.NORTH);
+        canvas2.add(canvas, BorderLayout.NORTH);
+        canvas2.add(text, BorderLayout.CENTER);
+        canvas2.add(browse, BorderLayout.LINE_END);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(canvas);
+        frame.setContentPane(canvas2);
+        
+    }
+
+    void display(){
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... a) {
+        final SimpleGUIWithFileChooser gui = new SimpleGUIWithFileChooser(new Controller());
+        gui.display();
+    }
 
 }
